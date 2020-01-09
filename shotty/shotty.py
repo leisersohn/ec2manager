5fdeee0e4c779c2ea6d32ea0f0605d61e4e91fb7
leisersohn <yaron.lirase@gmail.com>
2020-01-09 12:41:59 +0000

    Wait for instances to stop and start usign snapshot process

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -133,9 +133,21 @@ def create_snapshots(project):
 	instances = filter_instances(project)
 
 	for i in instances:
-			for v in i.volumes.all():
-				print("Creating snapshot of {0}".format(v.id))
-				v.create_snapshot(Description="Created by ec2manager")
+		print("Stopping {0}...".format(i.id))
+
+		i.stop()
+		i.wait_until_stopped()
+
+		for v in i.volumes.all():
+			print("  Creating snapshot of {0}".format(v.id))
+			v.create_snapshot(Description="Created by ec2manager")
+
+		print("Starting {0}...".format(i.id))
+
+		i.start()
+		i.wait_until_running()
+
+	print("Job's done!")
 
 	return
 
